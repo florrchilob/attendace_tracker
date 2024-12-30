4607a60c3d21c1759c850a8b2e8c2074d1f496e4
Flor compu Cundy <florchilob0703@gmail.com>
2024-12-30 19:26:06 +0200

    ver focus post enviar y seguir por enviar y todo tabla

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,70 +1,113 @@
 import React, { useEffect, useRef, useState } from "react";
 import TypingGif from '../assets/gifs/Typing.gif'
 import ScanningGif from '../assets/gifs/Scanning.gif'
+import Swal from 'sweetalert2';
+import '../App.css'
+
 
 const HomePage = () => {
   const [inputValue, setInputValue] = useState("");
   const inputRef = useRef(null);
-  const [selectedOption, setSelectedOption] = useState("misparIishi");
+  const [selectedOption, setSelectedOption] = useState("misparIshi");
 
   const onChangeInput = () => {
-    if (selectedOption == "misparIshi"){
-      if (inputRef.length > 11){
-        // alert
-      }
-      else{
-        setInputValue(inputRef)
-      }
+    if (selectedOption == "misparIshi" && inputRef.current.value.length > 253 || selectedOption == "tehudatZehut" && inputRef.current.value.length > 9){
+      Swal.fire({
+        position: "top",
+        icon: "error",
+        title: "בדוק את המספר שהוזן בבקשה",
+        showConfirmButton: false,
+        timer: 3500,
+        customClass: {
+          popup: "custom-popup",
+          title: "custom-title",
+        },
+      });
     }
-    if (selectedOption == "tehudatZehut"){
-      if (inputRef.length > 9){
-        // alert
-      }
-      else{
-        setInputValue(inputRef)
-      }
+    else{
+      setInputValue(inputRef.current.value)
     }
   }
 
   const handleSubmit = () => {
-    if (!inputValue.trim()) {
-      alert("Por favor, ingresa un número válido.");
-      return;
-    }
-
-    const response = { status: "ok" };
-
-    if (response.status === "ok") {
-      alert("Número guardado correctamente.");
-    } else if (response.status === "new") {
-      const fullName = prompt("Nuevo asistente. Por favor, ingresa el nombre completo:");
-      if (fullName) {
-        alert(`Nuevo asistente registrado: ${fullName}`);
+    if (selectedOption == "misparIshi" && inputValue.length < 6 || selectedOption == "tehudatZehut" && inputValue.length != 9) {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "בדוק את המספר שהוזן בבקשה",
+        showConfirmButton: false,
+        timer: 3500,
+        customClass: {
+          popup: "custom-popup",
+          title: "custom-title",
+        },
+      });
+      if (inputRef.current) {
+        inputRef.current.focus();
       }
-    } else {
-      alert("Error del sistema. Intenta nuevamente.");
+    }
+    else{
+      // llamar a api
+      let response = {"status": 200}
+      if (response.status == 200){
+        const now = new Date()
+        const hours = now.getHours();
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        const formattedTime = hours+":"+minutes
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "הגעתך נרשמה בהצלחה ב-" + formattedTime,
+          showConfirmButton: false,
+          timer: 2500,
+          customClass: {
+            popup: "custom-popup",
+            title: "custom-title-success",
+          },
+        }).then(()=> {
+          setInputValue("")
+          setSelectedOption("misparIshi")
+          setTimeout(() => {
+            console.log("Currently focused element:", document.activeElement);
+            if (inputRef.current) {
+              inputRef.current.focus();
+              console.log("Input focused:", inputRef.current === document.activeElement);
+            }
+          }, 100);
+        })
+      }
     }
   };
+
+  const onChangeSelect = (e) => {
+    if (e.target.value == "misparIshi" && inputValue > 11 || e.target.value == "tehudatZehut" && inputValue > 9){
+      setInputValue("")
+    }
+    setSelectedOption(e.target.value)
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }
 
   useEffect(() => {
     if (inputRef.current) {
       inputRef.current.focus();
     }
   }, []);
-
+  
   return (
     <div
       dir="rtl"
-      className="bg-bg-desktop bg-cover bg-center h-screen w-screen p-16 flex justify-center items-center"
+      className="bg-bg-desktop bg-cover bg-center h-screen w-screen p-16 flex justify-center items-center overflow-hidden"
     >      
     <div className="bg-gray-800 bg-opacity-90 rounded-3xl shadow-lg p-6 w-screen py-10 h-full px-auto py-auto items-center overflow-hidden">
       <h1 className="text-4xl font-bold text-white mb-8 text-center flex-row">איזה כיף שמישהבו הגיע!</h1>
       <div className="flex flex-row justify-center mt-16">
-        <div className="w-5/12 text-center flex justify-center flex-col items-center relative z-20">
+        <div className="w-5/12 text-center flex justify-center flex-col items-center relative z-20 transition-all duration-500">
           <select
             className="w-full mb-4 p-2 bg-gray-700 text-white rounded-lg h-12"
             value={selectedOption}
-            onChange={(e) => setSelectedOption(e.target.value)}
+            onChange={(e) => onChangeSelect(e)}
           >
             <option value="misparIshi">מספר אישי</option>
             <option value="tehudatZehut">תעודת זהות</option>
@@ -75,7 +118,7 @@ const HomePage = () => {
             className="w-full p-2 bg-gray-700 text-white rounded-lg mb-4 h-12"
             placeholder="הכנס מספר"
             value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
+            onChange={() => onChangeInput()}
           />
           <button
             onClick={handleSubmit}
@@ -85,11 +128,11 @@ const HomePage = () => {
           </button>
         </div>
       </div>
-      <div className="py-0 justify-between items-stretch flex flex-row h-auto overflow-hidden my-[-69px] z-0">
+      <div className="py-0 justify-between items-stretch flex flex-row h-auto overflow-hidden my-[-69px] z-0 ms-[-109px]">
         <div className="w-3/5 flex align-center flex-col justify-end items-start">
-          <img className="flex my-auto justify-end" src={ScanningGif} alt="Barcode Scanner" />
+          <img className="flex my-auto justify-end" src={ScanningGif} alt="Barcode Scanner"/>
         </div>
-        <div className="w-2/5 flex align-center flex-col justify-start items-end">
+        <div className="w-2/5 flex align-center flex-col justify-end items-end">
             <img className="flex my-auto justify-start" src={TypingGif} alt="Barcode Scanner" />
         </div>
         

d610d039e9b4d341a1378370df154e74f5f58b78
Flor Lob <florchilob0703@gmail.com>
2025-01-08 11:03:38 +0200

    ver sockets no con sockets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,14 @@ import { Route, Routes } from "react-router-dom"
 import { useLocation } from 'react-router-dom'
 import HomePage from "./pages/HomePage"
 import AttendeesPage from "./pages/AttendeesPage"
-import TestSocket from "./pages/SocketTest"
 
 function App() {
-  const location = useLocation().pathname
 
   return (
     <>
       <Routes>
         <Route path="/home" element={<HomePage/>}/>  
         <Route path='/attendees' element={<AttendeesPage/>}/>          
-        <Route path='/testing' element={<TestSocket/>}/>          
       </Routes>
     </>
   )

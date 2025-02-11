258685f9031db2f8de3077b8138c39fcdfd49c3c
Flor Lob <florchilob0703@gmail.com>
2025-02-11 13:52:04 +0200

    added the button logo to all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,33 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useNavigate  } from "react-router-dom"
 import { useLocation } from 'react-router-dom'
 import HomePage from "./pages/HomePage"
 import AttendeesPage from "./pages/AttendeesPage"
+import AkaIcon from "../src/assets/Logos/AkaIcon"
 
 function App() {
 
+  const navigate = useNavigate()
+
+  const handleNavigateHome = () => {
+    navigate('/home')
+  }
+
   return (
     <>
+      <button 
+        onClick={handleNavigateHome} 
+        className="absolute top-8 left-8 border-none cursor-pointer z-50 focus:outline-none 
+        rounded-full 
+        transition-all 
+        duration-300 
+        hover:shadow-lg 
+        hover:shadow-lavanderConvined"
+      >     
+        <AkaIcon />
+      </button>
       <Routes>
         <Route path="/home" element={<HomePage/>}/>  
-        <Route path='/attendees' element={<AttendeesPage/>}/>          
+        <Route path="/attendees" element={<AttendeesPage/>}/>          
       </Routes>
     </>
   )
---
+++ frontend/src/assets/Logos/AkaIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AkaIcon() {
+  return (
+    <div>
+      <img src="/aka.ico" alt="Home" style={{ width: '100px', height: '100px' }} />
+    </div>
+  )
+}
+
+export default AkaIcon
